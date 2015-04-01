f78a2002489149be5f5cf51e57964da23005b8ca
Rehan Butt <rehanbutt@cmu.edu>
2015-04-01 00:31:45 -0400

    Array of boxes
    sadly no circles :(
    :(
    :(
    :(

--- triad.js
+++ triad.js
@@ -1,13 +1,33 @@
 
 var background;
-  	function colorRand(){
+  	function colorRand()
+  	{
   		var index = Math.floor((Math.random() * names.length));
   		background = names[index][0];
   		document.getElementById("circle").style.background = "#"+names[index][0];
-  		console.log(background);
+  		//console.log(background);
   	}
-      function rotateBox(){
+      function rotateBox()
+      {
       	var w = $('#wheel').width()
       	$('#rotate').width(w/2)
+      	/*$('#rotate').css("rotate","90")*/
+      	var arr = new Array(20); //Makes array to house each of the rotation circles
+      	for (var i = 0; i <= arr.length - 1; i++) 
+      	{
+      		var elem = document.createElement('div');
+      		var cir = document.createElement('div');
+      		$(cir).css("background-color","#00ff00")
+      		elem.setAttribute("id", "rotate");
+      		arr[i] = elem;
+      		console.log(arr[i]);
+      		$(arr[i]).append(cir);
+      		$(arr[i]).css("rotate",(360/arr.length)*i);
+      		      		//should be able to simplify using a class rather than ID (Since Constant)
+      		$(arr[i]).css("width",w/2)
+      		$(arr[i]).css("height",50)
+      		      		//should be able to simplify using a class rather than ID (Since Constant)
 
+      		$("#wheel").append(arr[i]);
+      	};
       }
