2296b7b37d58e9cf93bafcc5312a256757ee30bb
Rehan Butt <rehanbutt@cmu.edu>
2015-03-31 20:16:10 -0400

    50% rotate

--- triad.js
+++ triad.js
@@ -1,12 +1,13 @@
+
 var background;
-  		function colorRand(){
-  			var index = Math.floor((Math.random() * names.length));
-  			background = names[index][0];
-  			document.getElementById("circle").style.background = "#"+names[index][0];
-  			console.log(background);
-  		}
+  	function colorRand(){
+  		var index = Math.floor((Math.random() * names.length));
+  		background = names[index][0];
+  		document.getElementById("circle").style.background = "#"+names[index][0];
+  		console.log(background);
+  	}
       function rotateBox(){
-      	var w = document.getElementById("wheel").style.width;
-        document.getElementById("rotate").style.width = w/2;
-        console.log(document.getElementById("rotate").style.width);
+      	var w = $('#wheel').width()
+      	$('#rotate').width(w/2)
+
       }
