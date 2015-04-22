3ee14c21c9ee7c8f76975137a37379dcffd20834
Rehan Butt <rehanbutt@cmu.edu>
2015-04-21 21:04:37 -0400

    Streamlined
    on route to library

--- triad.js
+++ triad.js
@@ -1,108 +1,149 @@
 
+var score = 0;
+var foundLeft = false;
+var foundRight = false;
 var background;
-  	function colorRand()
-  	{
-  		var index = Math.floor((Math.random() * names.length));
-  		background = names[index][0];
-  		document.getElementById("circle").style.background = "#"+names[index][0];
-  		return background;
-  	}
-    function colorRandNum()
+
+var arr = new Array(20); //Makes array to house each of the rotation circles
+
+var sel = new Array(0,10,11,12,13,14,16,17,18,19);
+var leftArr = new Array(16,17,18,19,0);
+var rightArr = new Array(10,11,12,13,14);
+var master = 5;
+
+
+function setThemAllToRed(arr,sel) {
+  for (var i = 0; i <= arr.length - 1; i++) 
+  {
+    if(isIn(i,sel))
     {
-      var index = Math.floor((Math.random() * names.length));
-      background = names[index][0];
-      return background;
+      $(arr[i]).children().css("background-color","#ff0000")
     }
-    function rotateBox()
-      {
-      	var w = $('#wheel').width()
-      	/*$('#rotate').css("rotate","90")*/
-      	var arr = new Array(20); //Makes array to house each of the rotation circles
-      	for (var i = 0; i <= arr.length - 1; i++) 
-      	{
-      		var elem = document.createElement('div');
-      		var cir = document.createElement('div');
-      		cir.setAttribute("id","circle");
-      		elem.setAttribute("id", "rotate");
-      		arr[i] = elem;
-      		//console.log(arr[i]);
-      		$(arr[i]).append(cir);
-      		$(arr[i]).css("rotate",(360/arr.length)*i);
-      		$(arr[i]).css("width",w/2)
-      		$("#wheel").append(arr[i]);
-      	};
+  };
+}
 
-      	var sel = new Array(0,10,11,12,13,14,16,17,18,19);
-        var leftArr = new Array(16,17,18,19,0);
-        var rightArr = new Array(10,11,12,13,14);
-      	var master = 5;
-      
+function colorize(arr,leftArr,rightArr,master){
+  var masterColor = colorRandNum();
+  var triadArray = tinycolor(masterColor).triad();//returns array [original,left,right]
 
-      	for (var i = 0; i <= arr.length - 1; i++) 
-      	{
-      		if(isIn(i,sel))
-      		{
-      			$(arr[i]).children().css("background-color","#ff0000")
-      		}
-      	};
-        var masterColor = colorRand();
-        var triadArray = tinycolor(masterColor).triad();//returns array [original,left,right]
-      	$(arr[master]).children().css ("background-color",masterColor)
-      	$(arr[master]).children().css("width",100)
-      	$(arr[master]).children().css("height",100)
-        var left = triadArray[1].toHexString();
-        var right = triadArray[2].toHexString();
+  $(arr[master]).children().css ("background-color",masterColor)
+  $(arr[master]).children().addClass("master")
+  $(arr[master]).children().addClass("shown")
 
-        //Returns a random integer between min (inclusive) and max (exclusive)
-        var leftIndex = Math.floor(Math.random() * (leftArr.length - 0)) + 0;
-        var rightIndex = Math.floor(Math.random() * (rightArr.length - 0)) + 0;
+  var left = triadArray[1].toHexString();
+  var right = triadArray[2].toHexString();
 
-        $(arr[leftArr[leftIndex]]).children().attr('id','leftAns');
-        $(arr[rightArr[rightIndex]]).children().attr('id','rightAns');
-        $(arr[leftArr[leftIndex]]).children().css("background-color",left);//16 - 19 & 0
-        $(arr[rightArr[rightIndex]]).children().css("background-color",right);//10 - 14
-        console.log("Left INDEX: "+leftIndex);//should not be higher than 4 (0-4)
-        console.log("Right INDEX: "+rightIndex);
-        console.log("Answer Left (Div #): "+leftArr[leftIndex]);
-        console.log("Answer Right (Div #): "+rightArr[rightIndex]);
+  //Returns a random integer between min (inclusive) and max (exclusive)
+  var leftAnsIndex = Math.floor(Math.random() * leftArr.length);
+  var rightAnsIndex = Math.floor(Math.random() * rightArr.length);
 
-        var leftOther = new Array(4);
-        var rightOther = new Array(4);
-        for (var i = 0; i <= 4; i++)
-        {
-          leftOther[i] = colorRandNum();
-          rightOther[i] = colorRandNum();
-        }
-        //var leftOther = tinycolor(left).analogous(4,25);
-        //var rightOther = tinycolor(right).analogous(4,35);
+  $(arr[leftArr[leftAnsIndex]]).children().attr('id','leftAns');
+  $(arr[rightArr[rightAnsIndex]]).children().attr('id','rightAns');
+  $(arr[leftArr[leftAnsIndex]]).children().css("background-color",left);//16 - 19 & 0
+  $(arr[rightArr[rightAnsIndex]]).children().css("background-color",right);//10 - 14
+  $(arr[leftArr[leftAnsIndex]]).children().addClass("shown");
+  $(arr[rightArr[rightAnsIndex]]).children().addClass("shown");
 
-        var counter = 0;//separate var to fix off my one when "if" is not executed
-        for (var i = 0; i<= leftArr.length; i++)
-        {
-          if (leftArr[leftIndex] != leftArr[i]) 
-          {
-            $(arr[leftArr[i]]).children().css("background-color",leftOther[counter])
-            counter++;
-          }
-        };
+  console.log("Left INDEX: "+leftAnsIndex);//should not be higher than 4 (0-4)
+  console.log("Right INDEX: "+rightAnsIndex);
+  console.log("Answer Left (Div #): "+leftArr[leftAnsIndex]);
+  console.log("Answer Right (Div #): "+rightArr[rightAnsIndex]);
 
-        counter = 0;
-        for (var i = 0; i<= rightArr.length; i++)
-        {
-          if (rightArr[rightIndex] != rightArr[i]) 
-          {
-            $(arr[rightArr[i]]).children().css("background-color",rightOther[counter])
-            counter++;
-          }
-        };
+  var leftOther = [];
+  var rightOther = [];
+  for (var i = 0; i < leftArr.length-1; i++)
+  {
+    leftOther[i] = colorRandNum();
+    rightOther[i] = colorRandNum();
+  }
 
-      }
+  var counter = 0;//separate var to fix off by one when "if" is not executed
+  for (var i = 0; i<= leftArr.length; i++)
+  {
+    if (leftArr[leftAnsIndex] != leftArr[i]) 
+    {
+      $(arr[leftArr[i]]).children().css("background-color",leftOther[counter])
+      $(arr[leftArr[i]]).children().addClass("shown")
+      counter++;
+    }
+  };
 
-      function isIn (i,sel) 
-      {
-      	for (var q = 0; q <= sel.length - 1; q++)
-      	{ 
-      		if (sel[q]==i) {return true};
-      	};
-      	return false;
-      }
+  counter = 0;
+  for (var i = 0; i<= rightArr.length; i++)
+  {
+    if (rightArr[rightAnsIndex] != rightArr[i]) 
+    {
+      $(arr[rightArr[i]]).children().css("background-color",rightOther[counter])
+      $(arr[rightArr[i]]).children().addClass("shown")
+      counter++;
+    }
+  };
+}
+
+/*function colorRand(){
+	var index = Math.floor((Math.random() * names.length));
+	background = names[index][0];
+	$("#circle").css("background", "#"+names[index][0]);
+	return background;
+}*/
+
+function colorRandNum(){
+  var index = Math.floor((Math.random() * names.length));
+  background = names[index][0];
+  return background;
+}
+
+function makeColorWheel(){
+	var w = $('#wheel').width()
+	for (var i = 0; i <= arr.length - 1; i++) 
+	{
+		var elem = document.createElement('div');
+		var cir = document.createElement('div');
+		$(cir).addClass("circle");
+		$(elem).addClass("rotate");
+		arr[i] = elem;
+		$(arr[i]).append(cir);
+		$(arr[i]).css("rotate",(360/arr.length)*i);
+		$(arr[i]).css("width",w/2)
+		$("#wheel").append(arr[i]);
+	};
+}
+
+function isIn (i,sel) {
+	for (var q = 0; q <= sel.length - 1; q++)
+	{ 
+		if (sel[q]==i) {return true};
+	};
+	return false;
+}
+
+function clickHandler() {
+  var myId = $(this).attr("id")
+
+  if (myId === "leftAns") {
+    console.log("I win on the left");
+    foundLeft = true;
+  }
+  else if (myId === "rightAns") {
+    console.log("I win on the right");
+    foundRight = true;
+  }
+  else {
+    console.log("I am a terrible loser.")
+  }
+
+  if (foundLeft && foundRight) {
+    console.log("you win");
+    score = score + 1;
+    foundRight = foundLeft = false;
+    console.log("i sohuld reset tings");
+
+  }
+}
+
+$(function() {
+  //colorRand();
+  makeColorWheel();
+  colorize(arr,leftArr,rightArr,master);
+  $('.circle').click(clickHandler);
+})
